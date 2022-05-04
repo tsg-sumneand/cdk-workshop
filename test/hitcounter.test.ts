6cec99fb0003a1af8a513f33f9beba272a66161b
Andrew Sumner <andrew_sumner@flutterint.com>
2022-05-04 15:52:12 +0100

    encryption change

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -7,20 +7,20 @@ test('DynamoDB Table Created With Encryption', () => {
     const stack = new cdk.Stack();
     // WHEN
     new HitCounter(stack, 'MyTestConstruct', {
-        downstream: new lambda.Function(stack, 'TestFunction', {
-            runtime: lambda.Runtime.NODEJS_14_X,
-            handler: 'hello.handler',
-            code: lambda.Code.fromAsset('lambda')
-        })
+      downstream:  new lambda.Function(stack, 'TestFunction', {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'hello.handler',
+        code: lambda.Code.fromAsset('lambda')
+      })
     });
     // THEN
     const template = Template.fromStack(stack);
     template.hasResourceProperties('AWS::DynamoDB::Table', {
-        SSESpecification: {
-            SSEEnabled: true
-        }
+      SSESpecification: {
+        SSEEnabled: true
+      }
     });
-});
+  });
 
 test('Lambda Has Environment Variables', () => {
     const stack = new cdk.Stack();
