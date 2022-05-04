25314aea47b96f6ea95c675958f0c7512fc211ef
Andrew Sumner <andrew_sumner@flutterint.com>
2022-05-04 16:11:55 +0100

    Set DynamoDB table RemovalPolicy to DESTROY.

--- lib/hitcounter.ts
+++ lib/hitcounter.ts
@@ -20,6 +20,7 @@ export class HitCounter extends Construct {
         super(scope, id);
 
         const table = new dynamodb.Table(this, 'Hits', {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
             partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
             encryption: dynamodb.TableEncryption.AWS_MANAGED,
         });
