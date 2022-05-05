32a67cad68a0d01dd1ef3c9448f90519ba075f0e
Andrew Sumner <andrew_sumner@flutterint.com>
2022-05-05 09:54:05 +0100

    add env.sh

---
+++ env.sh
@@ -0,0 +1,25 @@
+clear
+export AWS_PROFILE=aws-isp-fe-platform-dev
+export AWS_REGION=eu-west-1
+echo ""
+echo "AWS ENVIRONMENT VARIABLES"
+echo "========================="
+echo ""
+set | grep AWS_
+echo ""
+echo "GIT STATUS"
+echo "=========="
+echo ""
+git status
+echo ""
+echo "GIT BRANCH"
+echo "=========="
+echo ""
+git branch
+echo ""
+echo "AWS S3 LS"
+echo "========="
+echo ""
+aws s3 ls
+echo ""
+echo ""
