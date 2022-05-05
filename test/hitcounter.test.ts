fbea5321342e5e517d98f132272f3a79c2d152ed
Andrew Sumner <andrew_sumner@flutterint.com>
2022-05-05 11:12:58 +0100

    add validation test

--- test/hitcounter.test.ts
+++ test/hitcounter.test.ts
@@ -52,3 +52,18 @@ test('Lambda Has Environment Variables', () => {
         }
     );
 });
+
+test('read capacity can be configured', () => {
+  const stack = new cdk.Stack();
+
+  expect(() => {
+    new HitCounter(stack, 'MyTestConstruct', {
+      downstream: new lambda.Function(stack, 'TestFunction', {
+        runtime: lambda.Runtime.NODEJS_14_X,
+        handler: 'hello.handler',
+        code: lambda.Code.fromAsset('lambda')
+      }),
+      readCapacity: 3
+    });
+  }).toThrowError(/readCapacity must be greater than 5 and less than 20/);
+});
